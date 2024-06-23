6beb9236e4ffe68979cecbb0ab146dcd9240c361
Neil Ward <zooloo@me.com>
2024-06-23 16:48:53 -0700

    dangit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAuthContext } from "@/components/Auth/AuthContext";
 import ApiCheck from "@/components/apiCheck";
 import MyCharacters from "@/components/Character/myCharacters";
 import CharacterEquipment from "@/components/Item/characterEquipment";
 import InventorySearch from "@/components/Item/InventorySearch";
 
 export default function Home() {
+  const router = useRouter();
+  const { membershipId } = useAuthContext();
+
+  useEffect(() => {
+    if (!membershipId) {
+      router.push("/login");
+    }
+  }, [membershipId, router]);
+
+  if (!membershipId) {
+    return null; // or a loading spinner
+  }
+
   return (
     <main className="flex flex-col gap-2 pl-2 pr-2">
       <ApiCheck />
