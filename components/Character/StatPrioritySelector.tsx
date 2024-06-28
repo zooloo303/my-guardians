aad6b114b876f69d4c2262fd6964dc3401cebc59
Neil Ward <zooloo@me.com>
2024-06-28 14:47:45 -0700

    remove/replace dnd-kit

--- components/Character/StatPrioritySelector.tsx
+++ components/Character/StatPrioritySelector.tsx
@@ -6,35 +6,11 @@ import { SkeletonGuy } from "@/components/skeleton";
 import { SortableStatProps } from "@/lib/interfaces";
 import { defaultStatOrder } from "@/lib/destinyEnums";
 import { useManifestData } from "@/app/hooks/useManifest";
-import {
-  DndContext,
-  closestCenter,
-  KeyboardSensor,
-  PointerSensor,
-  useSensor,
-  useSensors,
-  DragEndEvent,
-} from "@dnd-kit/core";
-import {
-  arrayMove,
-  SortableContext,
-  sortableKeyboardCoordinates,
-  verticalListSortingStrategy,
-} from "@dnd-kit/sortable";
-import { useSortable } from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
+import { Reorder } from "framer-motion";
 
 const SortableStat: React.FC<SortableStatProps> = ({ id, icon, name }) => {
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id });
-
-  const style = {
-    transform: CSS.Transform.toString(transform),
-    transition,
-  };
-
   return (
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+    <Reorder.Item value={id} id={id}>
       <Badge className="flex items-center justify-between rounded-md mb-2 cursor-move overflow-hidden">
         <span className="flex items-center gap-2 whitespace-nowrap text-overflow-ellipsis overflow-hidden">
           <Image
@@ -46,20 +22,13 @@ const SortableStat: React.FC<SortableStatProps> = ({ id, icon, name }) => {
           {name}
         </span>
       </Badge>
-    </div>
+    </Reorder.Item>
   );
 };
 
 const StatPrioritySelector: React.FC = () => {
   const { data: manifestData } = useManifestData();
   const [statOrder, setStatOrder] = useState<string[]>(defaultStatOrder);
-
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    })
-  );
 
   if (!manifestData) {
     return <SkeletonGuy />;
@@ -70,40 +39,19 @@ const StatPrioritySelector: React.FC = () => {
     return <div>Stat definitions not found</div>;
   }
 
-  const handleDragEnd = (event: DragEndEvent) => {
-    const { active, over } = event;
-
-    if (active.id !== over?.id) {
-      setStatOrder((items) => {
-        const oldIndex = items.indexOf(active.id as string);
-        const newIndex = items.indexOf(over?.id as string);
-        return arrayMove(items, oldIndex, newIndex);
-      });
-    }
-  };
-
   return (
     <div className="max-w-lg p-2">
       <h2 className="text-lg mb-4">Stat Priority</h2>
-      <DndContext
-        sensors={sensors}
-        collisionDetection={closestCenter}
-        onDragEnd={handleDragEnd}
-      >
-        <SortableContext
-          items={statOrder}
-          strategy={verticalListSortingStrategy}
-        >
-          {statOrder.map((hash) => (
-            <SortableStat
-              key={hash}
-              id={hash}
-              icon={statDefinitions[hash].displayProperties.icon}
-              name={statDefinitions[hash].displayProperties.name}
-            />
-          ))}
-        </SortableContext>
-      </DndContext>
+      <Reorder.Group axis="y" values={statOrder} onReorder={setStatOrder}>
+        {statOrder.map((hash) => (
+          <SortableStat
+            key={hash}
+            id={hash}
+            icon={statDefinitions[hash].displayProperties.icon}
+            name={statDefinitions[hash].displayProperties.name}
+          />
+        ))}
+      </Reorder.Group>
     </div>
   );
 };
